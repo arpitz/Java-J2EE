8f304b591414fb94242bd00aa080b153dc15e866
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-06 14:51:48 +0530

    Web services using SOAP

---
+++ Module14EEWebServiceSOAP/src/com/edureka/Client/Client.java
@@ -0,0 +1,13 @@
+package com.edureka.Client;
+
+public class Client {
+	
+	public String sayHello(){
+		return "Hello from SOAP Web Services !!";
+	}
+	
+	public int usdToInr(int dollar){
+		return dollar * 75;
+	}
+
+}
